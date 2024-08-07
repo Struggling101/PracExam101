64a978712e6aff14c089ebfcdc126cf607002a88
Holly.Edwards <a1923627@adelaide.edu.au>
2024-08-07 14:46:14 +0930

    hopefully fixed??

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -3,6 +3,6 @@
 double* duplicateArray(double* array, int size) {
     
     double* array1 = 0;
-    array = array1;
+    *array = *array1;
     return array1;
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,5 +6,5 @@ int main() {
     double array[] = {1, 2, 3, 4};
     int size = sizeof(array) / sizeof(array[0]);
      std::cout << "Address of minimum value in array is: " << arrayMin(array, size) << std::endl;
-    return 1.2;
+    return 0;
 }
